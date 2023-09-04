93bd20d92b19cfffe8e5edd1c3dc3cc0e0455cc7
kmh824 <1rkdalsgur1@naver.com>
2023-09-04 23:07:35 +0900

    commit message

--- JavaProject.java
+++ JavaProject.java
@@ -1,3 +1,5 @@
+import java.util.Arrays;
+
 public class JavaProject {
 
     public static void main(String[] args) {
@@ -7,7 +9,7 @@ public class JavaProject {
             {4, 5, 6}
         };
 
-        System.out.println(array[0].length);
+        System.out.print(array[0].length);
     }
 }
 class ArrayCopyDemo {
@@ -22,5 +24,26 @@ class ArrayCopyDemo {
         for (String coffee : copyTo) {
             System.out.print(coffee + " ");
         }
+    }
+}
+
+class ArrayCopyOfDemo {
+    public static void main(String[] args) {
+        String[] copyFrom = {
+            "Affogato", "Americano", "Cappuccino", "Corretto", "Cortado",
+            "Doppio", "Espresso", "Frappucino", "Freddo", "Lungo", "Macchiato",
+            "Marocchino", "Ristretto" };
+        int[] c = {1, 2, 3};
+        String[] copyTo = java.util.Arrays.copyOfRange(copyFrom, 2, 7);
+        for (String coffee : copyTo) {
+            System.out.print(coffee + " ");
+        }
+
+        int index = java.util.Arrays.binarySearch(c, 1);
+        System.out.println(index);
+
+        boolean bool = java.util.Arrays.equals(copyFrom, copyTo);
+        java.util.Arrays.fill(c, 0);
+        Arrays.sort(c);
     }
 }
