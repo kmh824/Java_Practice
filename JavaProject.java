302a22af8a21e61b9c90f1ca6b7577271b8447fe
kmh824 <1rkdalsgur1@naver.com>
2023-09-04 19:15:17 +0900

    commit message

--- JavaProject.java
+++ JavaProject.java
@@ -1,6 +1,26 @@
 public class JavaProject {
 
     public static void main(String[] args) {
-        System.out.println("Hello");
+        //int[] anArray = new int[10];
+        int[][] array = {
+            {1, 2, 3},
+            {4, 5, 6}
+        };
+
+        System.out.println(array[0].length);
+    }
+}
+class ArrayCopyDemo {
+    public static void main(String[] args) {
+        String[] copyFrom = {
+            "Affogato", "Americano", "Cappuccino", "Corretto", "Cortado",
+            "Doppio", "Espresso", "Frappucino", "Freddo", "Lungo", "Macchiato",
+            "Marocchino", "Ristretto" };
+
+        String[] copyTo = new String[7];
+        System.arraycopy(copyFrom, 2, copyTo, 0, 7);
+        for (String coffee : copyTo) {
+            System.out.print(coffee + " ");
+        }
     }
 }
