5f3558fc7c0dfa262189e82d2bffe02cf5717ed6
kmh824 <1rkdalsgur1@naver.com>
2023-09-21 21:28:06 +0900

    commit message

--- JavaProject.java
+++ JavaProject.java
@@ -351,23 +351,62 @@ class JavaStudy8 {
     }
 }
  */
+class DataStructure {
 
-class Outer {
-    private int num = 55;
-    void m3() {
-        class Local{
-            void showNum(){
-                System.out.println(num);
-            }
+    // Create an array
+    private final static int SIZE = 15;
+    private int[] arrayOfInts = new int[SIZE];
+
+    public DataStructure() {
+        // fill the array with ascending integer values
+        for (int i = 0; i < SIZE; i++) {
+            arrayOfInts[i] = i;
         }
-        Local local = new Local();
-        local.showNum();
+    }
+
+    public void printEven() {
+
+        // Print out values of even indices of the array
+        DataStructureIterator iterator = this.new EvenIterator();
+        while (iterator.hasNext()) {
+            System.out.print(iterator.next() + " ");
+        }
+        System.out.println();
+    }
+
+    interface DataStructureIterator extends java.util.Iterator<Integer> { }
+
+    // Inner class implements the DataStructureIterator interface,
+    // which extends the Iterator<Integer> interface
+
+    private class EvenIterator implements DataStructureIterator {
+
+        // Start stepping through the array from the beginning
+        private int nextIndex = 0;
+
+        public boolean hasNext() {
+
+            // Check if the current element is the last in the array
+            return (nextIndex <= SIZE - 1);
+        }
+
+        public Integer next() {
+
+            // Record a value of an even index of the array
+            Integer retValue = Integer.valueOf(arrayOfInts[nextIndex]);
+
+            // Get the next even element
+            nextIndex += 2;
+            return retValue;
+        }
+    }
+
+    public static void main(String s[]) {
+
+        // Fill the array with integer values and print out only
+        // values of even indicesss
+        DataStructure ds = new DataStructure();
+        ds.printEven();
     }
 }
 
-public class JavaProject {
-    public static void main(String[] args) {
-        Outer outer = new Outer();
-        outer.m3();
-    }
-}
