5188505119c3367ca32b71e5a74abc8d5cf0a5b8
kmh824 <1rkdalsgur1@naver.com>
2023-09-22 21:58:49 +0900

    commit message

--- JavaProject.java
+++ JavaProject.java
@@ -404,7 +404,7 @@ class DataStructure {
     public static void main(String s[]) {
 
         // Fill the array with integer values and print out only
-        // values of even indicesss
+        // values of even indices
         DataStructure ds = new DataStructure();
         ds.printEven();
     }
